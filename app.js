241f8cee29a33598838b22b96a52df4a62ceaa38
RevanYor <70781398+RevanYor@users.noreply.github.com>
2023-08-05 23:48:16 -0400

    add form

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 let body = document.querySelector('body');
 let butt = document.querySelector('#butt');
+let form = document.querySelector('form');
+let ti = document.querySelector('#ti');
+let au = document.querySelector('#au');
+let pa = document.querySelector('#pa');
+let sub = document.querySelector('#sub');
+let formu = document.querySelector('#formu');
 
 let myLibrary = [];
 
 butt.addEventListener('click', () => {
-    console.log(sicp.info());
+    formu.classList.remove('dis');
+   /*  console.log(sicp.info());
+    let ti = prompt("Titulo:");
+    let au = prompt("Autor:");
+    let pa = prompt("Number of Pages:");
+    console.log(ti);
+    console.log(au);
+    console.log(pa); */
+})
+
+sub.addEventListener('click', () => {
+    console.log(ti.innerText);
 })
 
 //Constructor
