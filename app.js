8c0988a612733e26f8158b0223fc8553d2e51571
RevanYor <70781398+RevanYor@users.noreply.github.com>
2023-08-05 20:25:19 -0400

    first book

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let body = document.querySelector('body');
+
 let myLibrary = [];
 
 
@@ -6,20 +8,22 @@ function Book(title, author, pages, read) {
     this.author = author
     this.pages = pages
     this.read = read
-    this.info = function() {
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
-    }
 }
 
+Book.prototype.info = function() {
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
+}
 //console.log(theHobbit.info());
 
 const sicp = new Book('SCIP','Tony Stark','560', true)
 
+myLibrary.push(sicp);
+
 console.log(sicp.info());
 
-
-/* myLibrary.forEach((book) => {
+myLibrary.forEach((book) => {
     const bok = document.createElement('div');
     bok.classList.add('card');
+    bok.innerText = sicp.info();
     body.appendChild(bok);
-}); */
+});
