754d9886fdd92214d4b6e95013ddce5f86b644b1
RevanYor <70781398+RevanYor@users.noreply.github.com>
2023-08-05 22:39:54 -0400

    added button

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 let body = document.querySelector('body');
+let butt = document.querySelector('#butt');
 
 let myLibrary = [];
 
+butt.addEventListener('click', () => {
+    console.log(sicp.info());
+})
 
+//Constructor
 function Book(title, author, pages, read) {
     this.title = title
     this.author = author
@@ -13,14 +18,14 @@ function Book(title, author, pages, read) {
 Book.prototype.info = function() {
         return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
 }
-//console.log(theHobbit.info());
 
+//-----------------------------------------------------
 const sicp = new Book('SCIP','Tony Stark','560', true)
-
 myLibrary.push(sicp);
+//console.log(sicp.info());
+//-----------------------------------------------------
 
-console.log(sicp.info());
-
+//Add to Array
 myLibrary.forEach((book) => {
     const bok = document.createElement('div');
     bok.classList.add('card');
